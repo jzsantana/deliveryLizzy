41b05b4e641c0e562eb1e9e13b27513335ffe635
jzsantana <juliass7187@gmail.com>
2023-10-26 09:29:50 -0300

    atualizando

--- src/CardapioPrato.java
+++ src/CardapioPrato.java
@@ -1,17 +1,34 @@
+import Classes.Prato;
 import Components.ButtonVoltar;
 import Components.Tela;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class CardapioPrato extends Tela {
     public CardapioPrato(){
         super("src//imagens//escolhaPrato.jpg");
 
         ButtonVoltar btnVoltar = new ButtonVoltar();
+
+        ArrayList<Prato> listaPratos = Prato.pratos;
+        String [] opcoesPratos = new String[listaPratos.size()];
+        DefaultComboBoxModel<Prato> teste = new DefaultComboBoxModel<>(Prato.pratos.toArray(new Prato[0]));
+        JComboBox<Prato> comboBoxPrato = new JComboBox<>(teste);
+
+        comboBoxPrato.setBounds(50,360,350,35);
+        comboBoxPrato.setBackground(Color.WHITE);
+        comboBoxPrato.setForeground(Color.BLACK);
+        comboBoxPrato.setFont(new Font("Arial", Font.BOLD, 15));
         btnVoltar.setLocation(24,18);
+
         getContentPane().add(btnVoltar);
+        getContentPane().add(comboBoxPrato);
 
         btnVoltar.addActionListener(e -> {
-            SelUser selUser = new SelUser();
-            selUser.setVisible(true);
+            CardapioPrato cardPrato = new CardapioPrato();
+            cardPrato.setVisible(true);
             dispose();
         });
     }
